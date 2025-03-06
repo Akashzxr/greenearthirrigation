09a0668e1d499b60a3e63c71281355edab2e511d
akashks <akashks8855@gmail.com>
2025-03-06 18:51:26 +0530

    commit with irrigation page and gardening page

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@ function enqueue_custom_scripts()
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 
 
- wp_enqueue_style('swiper-css', get_template_directory_uri() . '/assets/swiper/swiper-bundle.min.css');
+wp_enqueue_style('swiper-css', get_template_directory_uri() . '/assets/swiper/swiper-bundle.min.css');
 wp_enqueue_script('swiper-js', get_template_directory_uri() . '/assets/swiper/swiper-bundle.min.js', array(), null, true);
- 
+
 
 /* gsap */
 function enqueue_gsap_local()
@@ -20,7 +20,6 @@ function enqueue_gsap_local()
 add_action('wp_enqueue_scripts', 'enqueue_gsap_local');
 
 
-
 /* custom gallery block */
 function myblock_gallery_block_block_init()
 {
@@ -29,9 +28,9 @@ function myblock_gallery_block_block_init()
 add_action('init', 'myblock_gallery_block_block_init');
 
 
-
 /* contact form submit */
-function handle_contact_form_submission() {
+function handle_contact_form_submission()
+{
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = sanitize_text_field($_POST['name']);
         $email = sanitize_email($_POST['email']);
@@ -70,16 +69,17 @@ add_action('admin_post_nopriv_send_contact_form', 'handle_contact_form_submissio
 
 
 /* other pages hero image changing option */
-function customize_hero_section($wp_customize) {
+function customize_hero_section($wp_customize)
+{
     // Add a Section for Hero Image
     $wp_customize->add_section('hero_section', array(
-        'title'    => __('Hero Section', 'yourtheme'),
+        'title' => __('Hero Section', 'yourtheme'),
         'priority' => 30,
     ));
 
     // Add a Setting for the Image
     $wp_customize->add_setting('hero_image', array(
-        'default'   => '',
+        'default' => '',
         'transport' => 'refresh',
         'sanitize_callback' => 'esc_url_raw'
     ));
@@ -89,10 +89,656 @@ function customize_hero_section($wp_customize) {
         $wp_customize,
         'hero_image',
         array(
-            'label'    => __('Upload Hero Image', 'yourtheme'),
-            'section'  => 'hero_section',
+            'label' => __('Upload Hero Image', 'yourtheme'),
+            'section' => 'hero_section',
             'settings' => 'hero_image'
         )
     ));
 }
 add_action('customize_register', 'customize_hero_section');
+
+
+
+/* acf code */
+add_action( 'acf/include_fields', function() {
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67c69ec044ad7',
+	'title' => 'about us',
+	'fields' => array(
+		array(
+			'key' => 'field_67c69ec04df19',
+			'label' => 'image_1',
+			'name' => 'image_1',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'about-template.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67c6a23a50af4',
+	'title' => 'contact',
+	'fields' => array(
+		array(
+			'key' => 'field_67c6a23b633e8',
+			'label' => 'image1',
+			'name' => 'image1',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'contact-template.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67c935088cb02',
+	'title' => 'gardening page',
+	'fields' => array(
+		array(
+			'key' => 'field_67c935090cd22',
+			'label' => 'gardening image 1',
+			'name' => 'gardening_image_1',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c9354b0cd23',
+			'label' => 'gardening image 2',
+			'name' => 'gardening_image_2',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c9354c0cd24',
+			'label' => 'gardening image 3',
+			'name' => 'gardening_image_3',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c9354f0cd25',
+			'label' => 'gardening image 4',
+			'name' => 'gardening_image_4',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c935510cd26',
+			'label' => 'gardening image 5',
+			'name' => 'gardening_image_5',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'gardening-template.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67c5612c1d08a',
+	'title' => 'home page',
+	'fields' => array(
+		array(
+			'key' => 'field_67c5612db50d0',
+			'label' => 'slider image 1',
+			'name' => 'slider_image_1',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c561aab50d2',
+			'label' => 'slider image 2',
+			'name' => 'slider_image_2',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c561e3d7fd8',
+			'label' => 'slider image 3',
+			'name' => 'slider_image_3',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c56b5bf7eac',
+			'label' => 'about side image',
+			'name' => 'about_side_image',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c69c7dee050',
+			'label' => 'youtube video link',
+			'name' => 'youtube_video_link',
+			'aria-label' => '',
+			'type' => 'url',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'allow_in_bindings' => 0,
+			'placeholder' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'home-template.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67c89c4ab9bee',
+	'title' => 'irrigation page',
+	'fields' => array(
+		array(
+			'key' => 'field_67c89c4b621e8',
+			'label' => 'slider_image_1',
+			'name' => 'slider_image_1',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c89c66621ea',
+			'label' => 'slider_image_2',
+			'name' => 'slider_image_2',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c89c68621eb',
+			'label' => 'slider_image_3',
+			'name' => 'slider_image_3',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c89c6d621ec',
+			'label' => 'slider_image_4',
+			'name' => 'slider_image_4',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c89c6f621ed',
+			'label' => 'slider_image_5',
+			'name' => 'slider_image_5',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'irrigation-template.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+
+	acf_add_local_field_group( array(
+	'key' => 'group_67c69e1d83a17',
+	'title' => 'services page',
+	'fields' => array(
+		array(
+			'key' => 'field_67c69e1dea6f4',
+			'label' => 'image_1',
+			'name' => 'image_1',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+		array(
+			'key' => 'field_67c69e32ea6f5',
+			'label' => 'image_2',
+			'name' => 'image_2',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 0,
+			'preview_size' => 'medium',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'services-template.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+} );
+
